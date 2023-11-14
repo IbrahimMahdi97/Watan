d6ee4c11efa73d778d5aac7099d7338f51b08c3d
Ibrahim Kadhim Mahdi <54472752+IbrahimMahdi97@users.noreply.github.com>
2023-11-14 17:12:01 +0300

    fix: update complaints table name

--- Repository/Query/ComplaintQuery.cs
+++ Repository/Query/ComplaintQuery.cs
@@ -2,13 +2,13 @@ namespace Repository.Query;
 
 public static class ComplaintQuery
 {
-    public const string AllComplaintsQuery = @"SELECT * FROM Complaint WHERE IsDeleted=0";
-    public const string ComplaintByIdQuery = @"SELECT * FROM Complaint WHERE Id=@Id";
+    public const string AllComplaintsQuery = @"SELECT * FROM Complaints WHERE IsDeleted=0";
+    public const string ComplaintByIdQuery = @"SELECT * FROM Complaints WHERE Id=@Id";
 
-    public const string InsertComplaintQuery = @"INSERT INTO Complaint (UserId, TypeId, Details, RecordDate) 
+    public const string InsertComplaintQuery = @"INSERT INTO Complaints (UserId, TypeId, Details, RecordDate) 
                                                 OUTPUT inserted.Id 
                                                 VALUES (@UserId, @TypeId, @Details, @RecordDate)";
 
-    public const string DeleteComplaintQuery = @"UPDATE Complaint SET IsDeleted=1 WHERE Id=@Id";
-    public const string UpdateComplaintQuery = @"UPDATE Complaint SET Details=@Details, TypeId=@TypeId WHERE Id=@Id";
+    public const string DeleteComplaintQuery = @"UPDATE Complaints SET IsDeleted=1 WHERE Id=@Id";
+    public const string UpdateComplaintQuery = @"UPDATE Complaints SET Details=@Details, TypeId=@TypeId WHERE Id=@Id";
 }
