358b26c20a4ccf4aab0299df62c7b5d3a2b5862c
Ibrahim Kadhim Mahdi <54472752+IbrahimMahdi97@users.noreply.github.com>
2023-11-15 14:46:00 +0300

    fix: update the table name in the query

--- Repository/Query/ComplaintTypeQuery.cs
+++ Repository/Query/ComplaintTypeQuery.cs
@@ -2,11 +2,11 @@ namespace Repository.Query;
 
 public static class ComplaintTypeQuery
 {
-    public const string AllComplaintTypesQuery = @"SELECT * FROM ComplaintType";
-    public const string ComplaintTypeByIdQuery = @"SELECT * FROM ComplaintType WHERE Id=@Id";
-    public const string InsertComplaintTypeQuery = @"INSERT INTO ComplaintType (Description, Prefix) 
+    public const string AllComplaintTypesQuery = @"SELECT * FROM ComplaintTypes";
+    public const string ComplaintTypeByIdQuery = @"SELECT * FROM ComplaintTypes WHERE Id=@Id";
+    public const string InsertComplaintTypeQuery = @"INSERT INTO ComplaintTypes (Description, Prefix) 
                                                     OUTPUT inserted.Id 
                                                     VALUES (@Description, @Prefix)";
 
-    public const string ComplaintTypeIdByPrefixQuery = @"SELECT Id FROM ComplaintType WHERE Prefix=@Prefix";
+    public const string ComplaintTypeIdByPrefixQuery = @"SELECT Id FROM ComplaintTypes WHERE Prefix=@Prefix";
 }
--- Repository/Query/PostTypeQuery.cs
+++ Repository/Query/PostTypeQuery.cs
@@ -2,11 +2,11 @@ namespace Repository.Query;
 
 public static class PostTypeQuery
 {
-    public const string AllPostTypesQuery = @"SELECT * FROM PostType";
-    public const string PostTypeByIdQuery = @"SELECT * FROM PostType WHERE Id=@Id";
-    public const string InsertPostTypeQuery = @"INSERT INTO PostType (Description, Prefix) 
+    public const string AllPostTypesQuery = @"SELECT * FROM PostTypes";
+    public const string PostTypeByIdQuery = @"SELECT * FROM PostTypes WHERE Id=@Id";
+    public const string InsertPostTypeQuery = @"INSERT INTO PostTypes (Description, Prefix) 
                                                     OUTPUT inserted.Id 
                                                     VALUES (@Description, @Prefix)";
 
-    public const string PostTypeIdByPrefixQuery = @"SELECT Id FROM PostType WHERE Prefix=@Prefix";
+    public const string PostTypeIdByPrefixQuery = @"SELECT Id FROM PostTypes WHERE Prefix=@Prefix";
 }
