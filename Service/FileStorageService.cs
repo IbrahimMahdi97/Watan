94baca1d3fec6af9d12590d869c5454b6edafd48
DESKTOP-LS0LKK6\MuntajabAlAni <muntajabalani98@gmail.com>
2023-11-19 16:02:27 +0300

    upload files extensions

--- Service/FileStorageService.cs
+++ Service/FileStorageService.cs
@@ -9,7 +9,8 @@ public class FileStorageService : IFileStorageService
     {
         if (entityId <= 0 || file is not { Length: > 0 }) return;
 
-        var filePath = Path.Combine(folderName, fileName ?? entityId + " - " + Guid.NewGuid());
+        var name = fileName ?? entityId + " - " + Guid.NewGuid();
+        var filePath = Path.Combine(folderName, name + Path.GetExtension(file.FileName));
 
         if (!Directory.Exists(folderName))
             Directory.CreateDirectory(folderName);
