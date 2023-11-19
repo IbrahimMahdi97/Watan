dc515359e64e7f038d10925c402b18bc43315cd0
Ibrahim Kadhim Mahdi <54472752+IbrahimMahdi97@users.noreply.github.com>
2023-11-19 15:30:49 +0300

    fix: inherate userdto from Manipulation

--- Shared/DataTransferObjects/UserDto.cs
+++ Shared/DataTransferObjects/UserDto.cs
@@ -1,15 +1,8 @@
 namespace Shared.DataTransferObjects;
 
-public class UserDto
+public class UserDto : UserForManipulationDto
 {
     public int Id { get; set; }
-    public string? FullName { get; set; }
-    public string? MotherName { get; set; }
-    public string? ProvinceOfBirth { get; set; }
-    public bool Gender { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string? PhoneNumber { get; set; }
-    public string? Email { get; set; }
     public IEnumerable<UserRoleDto>? Roles { get; set; }
     public DateTime RefreshTokenExpiryTime { get; set; }
     public string AccessToken { get; set; } = string.Empty;
