b5b7881181e3c4e8f8fc4262de53d271f9be5572
DESKTOP-LS0LKK6\MuntajabAlAni <muntajabalani98@gmail.com>
2023-12-02 09:54:15 +0300

    add notifications table and deviceId

---
+++ Watan/Migrations/AddNotificationsStuff_202312020001.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace Watan.Migrations;
+
+[Migration(202312020001)]
+public class AddNotificationsStuff_202312020001: Migration
+{
+    public override void Up()
+    {
+        Alter.Table("Users").AddColumn("DeviceId").AsString(int.MaxValue).Nullable();
+        
+        Create.Table("Notifications")
+            .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+            .WithColumn("Title").AsString(250).NotNullable()
+            .WithColumn("Body").AsString(int.MaxValue).NotNullable()
+            .WithColumn("Type").AsInt32().NotNullable() //Enum for Notifications Types
+            .WithColumn("ReferenceId").AsInt32().Nullable()
+            .WithColumn("UserId").AsInt32().NotNullable()
+            .ForeignKey("Users", "Id")
+            .WithColumn("RecordDate").AsDateTime2().WithDefault(SystemMethods.CurrentDateTime);
+    }
+
+    public override void Down()
+    {
+        Alter.Column("DeviceId").OnTable("Users");
+        Delete.Table("Notifications");
+    }
+}
