43c2c14a7499fcb04d62f30a79374af67fccf661
DESKTOP-LS0LKK6\MuntajabAlAni <muntajabalani98@gmail.com>
2023-12-02 10:11:18 +0300

    fix ExpiredOrMissedTokenMiddleware

--- Watan/Extensions/ExpiredOrMissedTokenMiddleware.cs
+++ Watan/Extensions/ExpiredOrMissedTokenMiddleware.cs
@@ -13,18 +13,25 @@ public class ExpiredOrMissedTokenMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
-        if (context.User.Identity is null || !context.User.Identity.IsAuthenticated)
+        if (context.Request.Path.Value != null)
         {
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = 401;
-            await context.Response.WriteAsync(new ErrorDetails
+            var path = context.Request.Path.Value.ToLower();
+            if (!path.Contains("login") && !path.Contains("refresh_token"))
             {
-                StatusCode = context.Response.StatusCode,
-                Message = "The provided token is expired or missing !"
-            }.ToString());
-            return;
+                if (context.User.Identity is null || !context.User.Identity.IsAuthenticated)
+                {
+                    context.Response.ContentType = "application/json";
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsync(new ErrorDetails
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = "The provided token is expired or missing !"
+                    }.ToString());
+                    return;
+                }
+            }
         }
-
+        
         await _next(context);
     }
 }
