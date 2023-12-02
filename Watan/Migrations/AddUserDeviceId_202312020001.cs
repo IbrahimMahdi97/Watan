10b495c88f29c5cf64a7ebfd96f1dbb96117a81f
DESKTOP-LS0LKK6\MuntajabAlAni <muntajabalani98@gmail.com>
2023-12-02 09:56:48 +0300

    fix notifications already added

---
+++ Watan/Migrations/AddUserDeviceId_202312020001.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace Watan.Migrations;
+
+[Migration(202312020001)]
+public class AddUserDeviceId_202312020001: Migration
+{
+    public override void Up()
+    {
+        Alter.Table("Users").AddColumn("DeviceId").AsString(int.MaxValue).Nullable();
+    }
+
+    public override void Down()
+    {
+        Alter.Column("DeviceId").OnTable("Users");
+    }
+}
