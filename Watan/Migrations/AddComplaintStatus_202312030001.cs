624dee35125d8ab68051c0e5576acb39e7ed7467
DESKTOP-LS0LKK6\MuntajabAlAni <muntajabalani98@gmail.com>
2023-12-03 16:47:01 +0300

    feat: add Status column to complaints table

---
+++ Watan/Migrations/AddComplaintStatus_202312030001.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace Watan.Migrations;
+
+[Migration(202312030001)]
+public class AddComplaintStatus_202312030001 : Migration
+{
+    public override void Up()
+    {
+        Alter.Table("Complaints").AddColumn("Status").AsInt32().NotNullable().WithDefaultValue(0);
+    }
+
+    public override void Down()
+    {
+        Alter.Column("Status").OnTable("Complaints");
+    }
+}
